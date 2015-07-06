da9d1f7275eef760e00dc8da3eb19ddb2a968c6e
Shikher <shikherverma@ymail.com>
2015-07-07 00:40:10 +0530

    added MaterialDrawer

--- app/src/main/java/com/romainpiel/titanic/library/Titanic.java
+++ app/src/main/java/com/romainpiel/titanic/library/Titanic.java
@@ -13,101 +13,123 @@ import android.view.animation.LinearInterpolator;
  * Date: 14/03/2014
  * Time: 09:34
  */
-public class Titanic {
+public class Titanic
+{
 
-    private AnimatorSet animatorSet;
-    private Animator.AnimatorListener animatorListener;
+  private AnimatorSet animatorSet;
+  private Animator.AnimatorListener animatorListener;
 
-    public Animator.AnimatorListener getAnimatorListener() {
-        return animatorListener;
-    }
+  public Animator.AnimatorListener getAnimatorListener()
+  {
+    return animatorListener;
+  }
 
-    public void setAnimatorListener(Animator.AnimatorListener animatorListener) {
-        this.animatorListener = animatorListener;
-    }
+  public void setAnimatorListener(Animator.AnimatorListener animatorListener)
+  {
+    this.animatorListener = animatorListener;
+  }
 
-    public void start(final TitanicTextView textView) {
+  public void start(final TitanicTextView textView)
+  {
 
-        final Runnable animate = new Runnable() {
-            @Override
-            public void run() {
+    final Runnable animate = new Runnable()
+    {
+      @Override
+      public void run()
+      {
 
-                textView.setSinking(true);
+        textView.setSinking(true);
 
-                // horizontal animation. 200 = wave.png width
-                ObjectAnimator maskXAnimator = ObjectAnimator.ofFloat(textView, "maskX", 0, 200);
-                maskXAnimator.setRepeatCount(ValueAnimator.INFINITE);
-                maskXAnimator.setDuration(500);
-                maskXAnimator.setStartDelay(0);
+        // horizontal animation. 200 = wave.png width
+        ObjectAnimator maskXAnimator = ObjectAnimator.ofFloat(textView, "maskX", 0, 200);
+        maskXAnimator.setRepeatCount(ValueAnimator.INFINITE);
+        maskXAnimator.setDuration(500);
+        maskXAnimator.setStartDelay(0);
 
-                int h = textView.getHeight();
+        int h = textView.getHeight();
 
-                // vertical animation
-                // maskY = 0 -> wave vertically centered
-                // repeat mode REVERSE to go back and forth
-                ObjectAnimator maskYAnimator = ObjectAnimator.ofFloat(textView, "maskY", h/2, - h/2);
-                maskYAnimator.setRepeatCount(0);
-                maskYAnimator.setRepeatMode(ValueAnimator.REVERSE);
-                maskYAnimator.setDuration(8000);
-                maskYAnimator.setStartDelay(0);
+        // vertical animation
+        // maskY = 0 -> wave vertically centered
+        // repeat mode REVERSE to go back and forth
+        ObjectAnimator maskYAnimator = ObjectAnimator.ofFloat(textView, "maskY", h / 2, -h / 2);
+        maskYAnimator.setRepeatCount(0);
+        maskYAnimator.setRepeatMode(ValueAnimator.REVERSE);
+        maskYAnimator.setDuration(8000);
+        maskYAnimator.setStartDelay(0);
 
-                // now play both animations together
-                animatorSet = new AnimatorSet();
-                animatorSet.playTogether(maskXAnimator, maskYAnimator);
-                animatorSet.setInterpolator(new LinearInterpolator());
-                animatorSet.addListener(new Animator.AnimatorListener() {
-                    @Override
-                    public void onAnimationStart(Animator animation) {
-                    }
+        // now play both animations together
+        animatorSet = new AnimatorSet();
+        animatorSet.playTogether(maskXAnimator, maskYAnimator);
+        animatorSet.setInterpolator(new LinearInterpolator());
+        animatorSet.addListener(new Animator.AnimatorListener()
+        {
+          @Override
+          public void onAnimationStart(Animator animation)
+          {
+          }
 
-                    @Override
-                    public void onAnimationEnd(Animator animation) {
-                        textView.setSinking(false);
+          @Override
+          public void onAnimationEnd(Animator animation)
+          {
+            textView.setSinking(false);
 
-                        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
-                            textView.postInvalidate();
-                        } else {
-                            textView.postInvalidateOnAnimation();
-                        }
-
-                        animatorSet = null;
-                    }
-
-                    @Override
-                    public void onAnimationCancel(Animator animation) {
-
-                    }
-
-                    @Override
-                    public void onAnimationRepeat(Animator animation) {
-
-                    }
-                });
-
-
-                if (animatorListener != null) {
-                    animatorSet.addListener(animatorListener);
-                }
-
-                animatorSet.start();
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN)
+            {
+              textView.postInvalidate();
             }
-        };
+            else
+            {
+              textView.postInvalidateOnAnimation();
+            }
 
-        if (!textView.isSetUp()) {
-            textView.setAnimationSetupCallback(new TitanicTextView.AnimationSetupCallback() {
-                @Override
-                public void onSetupAnimation(final TitanicTextView target) {
-                    animate.run();
-                }
-            });
-        } else {
-            animate.run();
-        }
-    }
+            animatorSet = null;
+          }
 
-    public void cancel() {
-        if (animatorSet != null) {
-            animatorSet.cancel();
+          @Override
+          public void onAnimationCancel(Animator animation)
+          {
+
+          }
+
+          @Override
+          public void onAnimationRepeat(Animator animation)
+          {
+
+          }
+        });
+
+
+        if (animatorListener != null)
+        {
+          animatorSet.addListener(animatorListener);
         }
+
+        animatorSet.start();
+      }
+    };
+
+    if (!textView.isSetUp())
+    {
+      textView.setAnimationSetupCallback(new TitanicTextView.AnimationSetupCallback()
+      {
+        @Override
+        public void onSetupAnimation(final TitanicTextView target)
+        {
+          animate.run();
+        }
+      });
     }
+    else
+    {
+      animate.run();
+    }
+  }
+
+  public void cancel()
+  {
+    if (animatorSet != null)
+    {
+      animatorSet.cancel();
+    }
+  }
 }
