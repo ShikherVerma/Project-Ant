4fac01a1014ff8c146128dc64fc9ef1d1030c2b2
Shikher <shikherverma@ymail.com>
2015-07-06 20:02:47 +0530

    made the splash screen

--- app/src/main/java/in/antaragni/antaragni/SplashActivity.java
+++ app/src/main/java/in/antaragni/antaragni/SplashActivity.java
@@ -1,42 +1,66 @@
 package in.antaragni.antaragni;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.widget.LinearLayout;
 
-public class SplashActivity extends AppCompatActivity
+import com.flaviofaria.kenburnsview.KenBurnsView;
+import com.romainpiel.titanic.library.Titanic;
+import com.romainpiel.titanic.library.TitanicTextView;
+
+public class SplashActivity extends Activity
 {
+  private static String TAG = SplashActivity.class.getName();
+  private static long SLEEP_TIME = 9;	// Sleep for some time
+  private KenBurnsView mImg;
 
   @Override
   protected void onCreate(Bundle savedInstanceState)
   {
     super.onCreate(savedInstanceState);
+    mImg = (KenBurnsView) findViewById(R.id.img);
     setContentView(R.layout.activity_splash);
+
+    LinearLayout hiddenLayout = (LinearLayout) findViewById(R.id.hidden);
+    TransitionDrawable transition = (TransitionDrawable) hiddenLayout.getBackground();
+    transition.startTransition(8000);
+
+    // Start timer and launch main activity
+    IntentLauncher launcher = new IntentLauncher();
+    launcher.start();
+
+    TitanicTextView tv = (TitanicTextView) findViewById(R.id.my_text_view);
+    // set fancy typeface
+    tv.setTypeface(Typefaces.get(this, "Satisfy-Regular.ttf"));
+
+    // start animation
+    new Titanic().start(tv);
+    tv.setShadowLayer(20, 0, 0, Color.YELLOW);
+
   }
 
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu)
-  {
-    // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(R.menu.menu_splash, menu);
-    return true;
-  }
+  private class IntentLauncher extends Thread {
 
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item)
-  {
-    // Handle action bar item clicks here. The action bar will
-    // automatically handle clicks on the Home/Up button, so long
-    // as you specify a parent activity in AndroidManifest.xml.
-    int id = item.getItemId();
+    @Override
+    /**
+     * Sleep for some time and than start new activity.
+     */
+    public void run() {
+      try {
+        // Sleeping
+        Thread.sleep(SLEEP_TIME*1000);
+      } catch (Exception e) {
+        Log.e(TAG, e.getMessage());
+      }
 
-    //noinspection SimplifiableIfStatement
-    if (id == R.id.action_settings)
-    {
-      return true;
+      // Start main activity
+      Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+      SplashActivity.this.startActivity(intent);
+      SplashActivity.this.finish();
     }
-
-    return super.onOptionsItemSelected(item);
   }
 }
