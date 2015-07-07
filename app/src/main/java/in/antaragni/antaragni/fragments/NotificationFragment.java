c975773d3eeec7614eb72ec80821ec04c5764cbd
Shikher <shikherverma@ymail.com>
2015-07-07 11:53:39 +0530

    added recycler view to Notification Screen

--- app/src/main/java/in/antaragni/antaragni/fragments/NotificationFragment.java
+++ app/src/main/java/in/antaragni/antaragni/fragments/NotificationFragment.java
@@ -2,11 +2,14 @@ package in.antaragni.antaragni.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import in.antaragni.antaragni.R;
+import in.antaragni.antaragni.adapters.SampleListAdapter;
 
 /**
  * A simple {@link android.support.v4.app.Fragment} subclass.
@@ -33,6 +36,10 @@ public class NotificationFragment extends Fragment {
   public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
     // Inflate the layout for this fragment
     // don't look at this layout it's just a listView to show how to handle the keyboard
-    return inflater.inflate(R.layout.fragment_notification, container, false);
+    View view = inflater.inflate(R.layout.fragment_notification, container, false);
+    RecyclerView rv = (RecyclerView) view.findViewById(R.id.sample_recycler_view);
+    rv.setLayoutManager(new LinearLayoutManager(getActivity()));
+    rv.setAdapter(new SampleListAdapter());
+    return view ;
   }
 }
