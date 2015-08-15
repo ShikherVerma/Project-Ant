435e5cb5fe16e3c1cdb7b07bd43dedcc83b1561f
Shikher <shikherverma@ymail.com>
2015-08-15 21:55:35 +0530

    some map work done

--- app/src/main/java/in/antaragni/antaragni/fragments/RecyclerViewFragment.java
+++ app/src/main/java/in/antaragni/antaragni/fragments/RecyclerViewFragment.java
@@ -12,21 +12,16 @@ import android.widget.TextView;
 import in.antaragni.antaragni.R;
 import in.antaragni.antaragni.adapters.SampleListAdapter;
 
-/**
- * Created by shikher on 29/6/15.
- */
 public class RecyclerViewFragment extends Fragment
 {
   // Store instance variables
-  private String title;
   private int page;
 
   // newInstance constructor for creating fragment with arguments
-  public static RecyclerViewFragment newInstance(int page, String title) {
+  public static RecyclerViewFragment newInstance(int page) {
     RecyclerViewFragment fragmentFirst = new RecyclerViewFragment();
     Bundle args = new Bundle();
     args.putInt("someInt", page);
-    args.putString("someTitle", title);
     fragmentFirst.setArguments(args);
     return fragmentFirst;
   }
@@ -36,7 +31,6 @@ public class RecyclerViewFragment extends Fragment
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     page = getArguments().getInt("someInt", 0);
-    title = getArguments().getString("someTitle");
   }
 
   // Inflate the view for the fragment based on layout XML
@@ -44,8 +38,6 @@ public class RecyclerViewFragment extends Fragment
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
                            Bundle savedInstanceState) {
     View view = inflater.inflate(R.layout.fragment_recycler_view, container, false);
-    TextView tvLabel = (TextView) view.findViewById(R.id.tvLabel);
-    tvLabel.setText(page + " -- " + title);
 
     RecyclerView rv = (RecyclerView) view.findViewById(R.id.sample_recycler_view);
     rv.setLayoutManager(new LinearLayoutManager(getActivity()));
