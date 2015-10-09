a03a7c867a8ed27d4447ce06e5495ae24752767c
Shikher <shikherverma@ymail.com>
2015-10-09 19:45:55 +0530

    added reminder bottom sheet

--- app/src/main/java/in/antaragni/ant/fragments/inner/DayViewFragment.java
+++ app/src/main/java/in/antaragni/ant/fragments/inner/DayViewFragment.java
@@ -1,9 +1,16 @@
 package in.antaragni.ant.fragments.inner;
 
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.CalendarContract;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -11,6 +18,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.cocosw.bottomsheet.BottomSheet;
 import com.nhaarman.listviewanimations.appearance.AnimationAdapter;
 import com.nhaarman.listviewanimations.appearance.simple.SwingBottomInAnimationAdapter;
 
@@ -33,6 +41,13 @@ public class DayViewFragment extends Fragment
   // Store instance variables
   private int mDay;
   private DatabaseAccess databaseAccess;
+  private long mGoogleCalendarNumber = -1;
+
+  // The indices for the projection array above.
+  private static final int PROJECTION_ID_INDEX = 0;
+  private static final int PROJECTION_ACCOUNT_NAME_INDEX = 1;
+  private static final int PROJECTION_DISPLAY_NAME_INDEX = 2;
+  private static final int PROJECTION_OWNER_ACCOUNT_INDEX = 3;
 
   // newInstance constructor for creating fragment with arguments
   public static DayViewFragment newInstance(int day)
@@ -50,7 +65,45 @@ public class DayViewFragment extends Fragment
   {
     super.onCreate(savedInstanceState);
     mDay = getArguments().getInt("Day Number", 1);
+    // Run query
+    Cursor cur = null;
+    ContentResolver cr = getActivity().getContentResolver();
+    Uri uri = CalendarContract.Calendars.CONTENT_URI;
+    String selection = "(" + CalendarContract.Calendars.ACCOUNT_TYPE + " = ?)";
+    String[] selectionArgs = new String[]{"com.google"};
+    // Submit the query and get a Cursor object back.
+    cur = cr.query(uri, EVENT_PROJECTION, selection, selectionArgs, null);
+    Log.i("ant calendar", " attempting to read calendar" + mDay);
+    while (cur.moveToNext())
+    {
+      long calID = 0;
+      String displayName = null;
+      String accountName = null;
+      String ownerName = null;
+
+      // Get the field values
+      calID = cur.getLong(PROJECTION_ID_INDEX);
+      displayName = cur.getString(PROJECTION_DISPLAY_NAME_INDEX);
+      accountName = cur.getString(PROJECTION_ACCOUNT_NAME_INDEX);
+      ownerName = cur.getString(PROJECTION_OWNER_ACCOUNT_INDEX);
+      Log.i("ant calendar", " day :" + mDay + " data : " + calID + "  " + accountName + "  " + displayName + "  " + ownerName);
+      if (mGoogleCalendarNumber == -1)
+      {
+        Log.i("ant calendar", " calendar id is " + calID );
+        mGoogleCalendarNumber = calID;
+      }
+    }
+    cur.close();
   }
+
+  // Projection array. Creating indices for this array instead of doing
+  // dynamic lookups improves performance.
+  public static final String[] EVENT_PROJECTION = new String[]{
+    CalendarContract.Calendars._ID,                           // 0
+    CalendarContract.Calendars.ACCOUNT_NAME,                  // 1
+    CalendarContract.Calendars.CALENDAR_DISPLAY_NAME,         // 2
+    CalendarContract.Calendars.OWNER_ACCOUNT                  // 3
+  };
 
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -102,7 +155,53 @@ public class DayViewFragment extends Fragment
         @Override
         public void onMenuItemClick(BaseCard card, MenuItem item)
         {
-          Toast.makeText(getActivity(), "Click on card menu" + event.getName() + " item=" + item.getTitle(), Toast.LENGTH_SHORT).show();
+          final String REMINDER = "Set Reminder";
+          final String MAP = "View Map";
+          switch (item.getTitle().toString())
+          {
+            case REMINDER:
+              if (mGoogleCalendarNumber != -1)
+              {
+                new BottomSheet.Builder(getActivity()).title("Reminder me").sheet(R.menu.reminder_menu).listener(new DialogInterface.OnClickListener()
+                {
+                  @Override
+                  public void onClick(DialogInterface dialog, int which)
+                  {
+                    switch (which)
+                    {
+                      case R.id.min5:
+                        setReminder(5);
+                        break;
+                      case R.id.min10:
+                        setReminder(10);
+                        break;
+                      case R.id.min30:
+                        setReminder(30);
+                        break;
+                    }
+                  }
+                }).show();
+              } else
+              {
+                Calendar beginTime = Calendar.getInstance();
+                beginTime.set(2015, event.getStart_time().get(Calendar.MONTH), event.getStart_time().get(Calendar.DATE), event.getStart_time().get(Calendar.HOUR), event.getStart_time().get(Calendar.MINUTE));
+                Log.i("ant calendar", event.getStart_time().get(Calendar.MONTH) + "  " + event.getStart_time().get(Calendar.DATE) +"  " + event.getStart_time().get(Calendar.HOUR) +"  " + event.getStart_time().get(Calendar.MINUTE));
+                Calendar endTime = Calendar.getInstance();
+                endTime.set(2015, event.getEnd_time().get(Calendar.MONTH), event.getEnd_time().get(Calendar.DATE), event.getEnd_time().get(Calendar.HOUR), event.getEnd_time().get(Calendar.MINUTE));
+                Log.i("ant calendar", event.getEnd_time().get(Calendar.MONTH) + "  " + event.getEnd_time().get(Calendar.DATE) + "  " + event.getEnd_time().get(Calendar.HOUR) + "  " + event.getEnd_time().get(Calendar.MINUTE));
+                Intent intent = new Intent(Intent.ACTION_INSERT)
+                  .setData(CalendarContract.Events.CONTENT_URI)
+                  .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, beginTime.getTimeInMillis())
+                  .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, endTime.getTimeInMillis())
+                  .putExtra(CalendarContract.Events.TITLE, event.getName())
+                  .putExtra(CalendarContract.Events.ALLOWED_REMINDERS, 1)
+                  .putExtra(CalendarContract.Events.DESCRIPTION, event.getDescription());
+                startActivity(intent);
+              }
+              break;
+            case MAP:
+              break;
+          }
         }
       });
       addCardHeader(header);
@@ -120,6 +219,39 @@ public class DayViewFragment extends Fragment
           context.startActivity(intent);
         }
       });
+    }
+
+    public void setReminder(int min)
+    {
+      long startMillis = 0;
+      long endMillis = 0;
+      Calendar beginTime = Calendar.getInstance();
+      beginTime.set(2015,event.getStart_time().get(Calendar.MONTH) - 1 , event.getStart_time().get(Calendar.DATE), event.getStart_time().get(Calendar.HOUR), event.getStart_time().get(Calendar.MINUTE));
+      Log.i("ant calendar", event.getStart_time().get(Calendar.MONTH) + "  " + event.getStart_time().get(Calendar.DATE) + "  " + event.getStart_time().get(Calendar.HOUR) + "  " + event.getStart_time().get(Calendar.MINUTE));
+      startMillis = beginTime.getTimeInMillis();
+      Log.i("ant calendar", event.getEnd_time().get(Calendar.MONTH)  + "  " + event.getEnd_time().get(Calendar.DATE) + "  " + event.getEnd_time().get(Calendar.HOUR) + "  " + event.getEnd_time().get(Calendar.MINUTE));
+      Calendar endTime = Calendar.getInstance();
+      endTime.set(2015 , event.getEnd_time().get(Calendar.MONTH) - 1, event.getEnd_time().get(Calendar.DATE), event.getEnd_time().get(Calendar.HOUR), event.getEnd_time().get(Calendar.MINUTE));
+      endMillis = endTime.getTimeInMillis();
+
+      ContentResolver cr = getActivity().getContentResolver();
+      ContentValues values = new ContentValues();
+      values.put(CalendarContract.Events.DTSTART, startMillis);
+      values.put(CalendarContract.Events.DTEND, endMillis);
+      values.put(CalendarContract.Events.TITLE, event.getName());
+      values.put(CalendarContract.Events.DESCRIPTION, event.getDescription());
+      values.put(CalendarContract.Events.CALENDAR_ID, mGoogleCalendarNumber);
+      values.put(CalendarContract.Events.EVENT_TIMEZONE, "Asia/Calcutta");
+      Uri uri = cr.insert(CalendarContract.Events.CONTENT_URI, values);
+      // get the event ID that is the last element in the Uri
+      long eventID = Long.parseLong(uri.getLastPathSegment());
+      cr = getActivity().getContentResolver();
+      values = new ContentValues();
+      values.put(CalendarContract.Reminders.MINUTES, min);
+      values.put(CalendarContract.Reminders.EVENT_ID, eventID);
+      values.put(CalendarContract.Reminders.METHOD, CalendarContract.Reminders.METHOD_ALERT);
+      uri = cr.insert(CalendarContract.Reminders.CONTENT_URI, values);
+      Toast.makeText(getActivity(), "Reminder set for " + min + " minutes before event", Toast.LENGTH_SHORT).show();
     }
 
     @Override
