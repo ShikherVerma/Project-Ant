b6b96c999e0d1ce29a03f81d8563ff0491336ebf
Shikher <shikherverma@ymail.com>
2015-10-29 08:42:28 +0530

    added notification screen

--- app/src/main/java/in/antaragni/ant/datahandler/DatabaseAccess.java
+++ app/src/main/java/in/antaragni/ant/datahandler/DatabaseAccess.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 import com.google.android.gms.maps.model.LatLng;
 
